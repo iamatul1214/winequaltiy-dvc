2097ffd2c5116dd97ffd1e66e626de7e62bd48dc
atulkumarrai <iamatul1214@gmail.com>
2021-11-07 12:48:19 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wineQuality", version="0.0.1",
+    description="It is a wine quality package with MLOps",
+    author="Atul Rai",
+    packages=find_packages(),
+    license="MIT"
+)
